6e2331226f483df1415b371a6befb93356bf8a8f
Parvesh96 <parvesh.kumar@bacancy.com>
2022-01-04 15:13:23 +0530

    Heroku Deply Changes

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const {fetchLatLong,fetchCurrentWeather} = require('./Utils/functions.js')
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const publicDirectory = path.join(__dirname,'../public')
 
